dc00361d69145c3e72477b81e0b8a93e0e994938
handlename <nagata@handlena.me>
2017-06-11 16:06:38 +0900

    add tests for reflect configurations.

--- config_test.go
+++ config_test.go
@@ -1,6 +1,9 @@
 package mdqi
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 )
@@ -27,5 +30,87 @@ func TestConfigFromFile(t *testing.T) {
 	if !reflect.DeepEqual(conf, expected) {
 		t.Errorf("unexpected conf: %+v", conf)
 		t.Logf("  expected conf: %+v", expected)
+	}
+}
+
+func TestConfigSetMdqBin(t *testing.T) {
+	// invalid path
+	{
+		conf := Conf{Mdq: ConfMdq{Bin: "/path/to/mdq"}}
+
+		_, err := NewApp(conf)
+		if err == nil {
+			t.Error("should be error")
+		}
+	}
+
+	// valid path
+	{
+		wd, _ := os.Getwd()
+		conf := Conf{Mdq: ConfMdq{Bin: filepath.Join(wd, "test", "bin", "mdq")}}
+
+		app, err := NewApp(conf)
+		if err != nil {
+			t.Error("unexpected error:", err)
+		}
+
+		if p := app.mdqPath; p != conf.Mdq.Bin {
+			t.Error("failed to set mdq path:", p)
+		}
+	}
+}
+
+func TestConfigSetMdqConfig(t *testing.T) {
+	conf := Conf{Mdq: ConfMdq{Config: "/path/to/conf.yaml"}}
+
+	app, err := NewApp(conf)
+	if err != nil {
+		t.Error("unexpected error:", err)
+	}
+
+	if p := app.mdqConfigPath; p != conf.Mdq.Config {
+		t.Error("unexpected mdq config path:", p)
+	}
+}
+
+func TestConfigSetMdqiHistory(t *testing.T) {
+	tmp, _ := ioutil.TempFile("", "mdqi")
+	conf := Conf{Mdqi: ConfMdqi{History: tmp.Name()}}
+
+	app, err := NewApp(conf)
+	if err != nil {
+		t.Error("unexpected error:", err)
+	}
+
+	if p := app.historyPath; p != conf.Mdqi.History {
+		t.Error("unexpected history path:", p)
+	}
+}
+
+func TestConfigSetMdqiDefaultTag(t *testing.T) {
+	conf := Conf{Mdqi: ConfMdqi{DefaultTag: "db1"}}
+
+	app, err := NewApp(conf)
+	if err != nil {
+		t.Error("unexpected error:", err)
+	}
+
+	if tag := app.tag; tag != "db1" {
+		t.Error("unexpected tag:", tag)
+	}
+}
+
+func TestConfigSetMdqiDefaultDisplay(t *testing.T) {
+	conf := Conf{Mdqi: ConfMdqi{DefaultDisplay: "vertical"}}
+
+	app, err := NewApp(conf)
+	if err != nil {
+		t.Error("unexpected error:", err)
+	}
+
+	switch app.printer.(type) {
+	case VerticalPrinter:
+	default:
+		t.Errorf("unexpected printer %s:", app.printer)
 	}
 }
