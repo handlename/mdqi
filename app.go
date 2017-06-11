handlename/mdqi

commit f91b046dad866fd0c9293ddb1f285f327b78f4a8

supports multiline input.

app.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/user"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"os/exec"
@@ -175,6 +176,12 @@ func (app *App) runLiner() {
 
 	app.initHistory(line)
 
+	rgxFinishLine := regexp.MustCompile(";$")
+	lineFinished := true
+
+	var l string
+	var err error
+
 LOOP:
 	for {
 		if !app.Alive {
@@ -182,11 +189,23 @@ LOOP:
 			break LOOP
 		}
 
-		l, err := line.Prompt("mdq> ")
+		if lineFinished {
+			l, err = line.Prompt("mdq> ")
+		} else {
+			var ll string
+			ll, err = line.Prompt("   | ")
+			l = strings.Join([]string{l, ll}, " ")
+		}
 
 		switch err {
 		case nil:
 			l = strings.Trim(l, " \n")
+
+			if lineFinished = rgxFinishLine.MatchString(l); lineFinished {
+			} else {
+				// If line is not finished, read next line as continue.
+				continue
+			}
 
 			if l == "" {
 				continue

commit fad791f7e5805e7e5d1a0b3e4cd5948687b0be4b

fix slash commands are not realized.
caused by f91b046dad866fd0c9293ddb1f285f327b78f4a8

app.go
@@ -205,17 +205,9 @@ LOOP:
 		case nil:
 			input = strings.Trim(input, " \n")
 
-			if lineFinished = rgxLineFinisehd.MatchString(input); lineFinished {
-			} else {
-				// If line is not finished, read next line as continue.
-				continue
-			}
-
 			if input == "" {
 				continue
 			}
-
-			line.AppendHistory(input)
 
 			// run slash command
 			scmd, _ := ParseSlashCommand(input)
@@ -223,6 +215,14 @@ LOOP:
 				app.runSlashCommand(scmd)
 				continue
 			}
+
+			if lineFinished = rgxLineFinisehd.MatchString(input); lineFinished {
+			} else {
+				// If line is not finished, read next line as continue.
+				continue
+			}
+
+			line.AppendHistory(input)
 
 			// run mdq
 			results, err := app.RunCmd(input, app.buildCmdArgs()...)